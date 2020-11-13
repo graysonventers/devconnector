8c68b67be3b5d192754f261a4236dc3fbd8a1541
Grayson Venters <graysonventers@gmail.com>
2020-11-13 10:05:16 -0800

    small edit for test push

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,5 +1,4 @@
 //  Parent component for individual profile pages
-
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
@@ -23,7 +22,7 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
                 <Link to="/profiles" className="btn btn-light"> Back To Profiles
                 </Link>
                 {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to="/edit-profile" className="btn btn-dark">Edit Profile</Link>)}
-                <div class="profile-grid my-1">
+                <div className="profile-grid my-1">
                     <ProfileTop profile={profile} />
                     <ProfileAbout profile={profile} />
                     <div className="profile-exp bg-white p-2">
@@ -34,8 +33,8 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
                             ))}
                         </Fragment>) : (<h4>No experience credentials</h4>)}
                         </div>
-                    <div class="profile-edu bg-white p-2">
-                        <h2 class="text-primary">Education</h2>
+                    <div className="profile-edu bg-white p-2">
+                        <h2 className="text-primary">Education</h2>
                         {profile.education.length > 0 ? (<Fragment>
                             {profile.education.map(education => (
                                 <ProfileEducation key={education._id} education={education}/>
